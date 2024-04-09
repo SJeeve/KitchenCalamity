ee20f3b45add403f4b9d77e27343428bdab72ae7
Owen Smith <s112672@ltisdschools.net>
2024-04-09 15:56:44 -0500

    Basic random room generation

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -4,15 +4,116 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject roomPrefab;
+    //Max rooms and min rooms count for dungeon generation
+    [SerializeField] private int maxRooms = 15;
+    [SerializeField] private int minRooms = 10;
+
+    //Size of each room
+    int roomWidth = 20;
+    int roomHeight = 12;
+
+    //Size of grid that contains the rooms
+    int gridSizeX = 10;
+    int gridSizeY = 10;
+
+    private List<GameObject> roomObjects = new List<GameObject>();
+
+    private Queue<Vector2Int> roomQueue = new Queue<Vector2Int>();
+
+    //All new rooms spawned inside of this grid
+    private int[,] roomGrid;
+
+    private int roomCount;
+
+    private bool generationComplete = false;
+
+    private void Start()
     {
-        
+        roomGrid = new int[gridSizeX, gridSizeY];
+        roomQueue = new Queue<Vector2Int>();
+
+        //Causes room generation to start from middle of room
+        Vector2Int initialRoomIndex = new Vector2Int(gridSizeX / 2, gridSizeY / 2);
+        StartRoomGenerationFromRoom(initialRoomIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        
+        if(roomQueue.Count > 0 && roomCount < maxRooms && !generationComplete) 
+        {
+            Vector2Int roomIndex = roomQueue.Dequeue();
+            int gridX = roomIndex.x;
+            int gridY = roomIndex.y;
+
+            TryGenerateRoom(new Vector2Int(gridX - 1, gridY));
+            TryGenerateRoom(new Vector2Int(gridX + 1, gridY));
+            TryGenerateRoom(new Vector2Int(gridX, gridY + 1));
+            TryGenerateRoom(new Vector2Int(gridX, gridY - 1));
+        } else if (!generationComplete)
+        {
+            Debug.Log($"Generation complete, {roomCount} rooms created");
+            generationComplete = true;
+        }
+    }
+    //Starts generation of rooms
+    private void StartRoomGenerationFromRoom(Vector2Int roomIndex)
+    {
+        roomQueue.Enqueue(roomIndex);
+        int x = roomIndex.x;
+        int y = roomIndex.y;
+        roomGrid[x, y] = 1;
+        roomCount++;
+        var initialRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(roomIndex), Quaternion.identity);
+        initialRoom.name = $"Room-{roomCount}";
+        initialRoom.GetComponent<Room>().RoomIndex = roomIndex;
+        roomObjects.Add(initialRoom);
+    }
+
+
+    private bool TryGenerateRoom(Vector2Int roomIndex)
+    {
+        int x = roomIndex.x;
+        int y = roomIndex.y;
+
+        if (roomCount >= maxRooms)
+            return false;
+
+        if (Random.value < 0.5f && roomIndex != Vector2Int.zero)
+            return false;
+        roomQueue.Enqueue(roomIndex);
+        roomGrid[x, y] = 1;
+        roomCount++;
+
+        var newRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(roomIndex), Quaternion.identity);
+        newRoom.GetComponent<Room>().RoomIndex = roomIndex;
+        newRoom.name = $"Room-{roomCount}";
+        roomObjects.Add(newRoom);
+
+        return true;
+    }
+    private Vector3 GetPositionFromGridIndex(Vector2Int gridIndex)
+    {
+        int gridX = gridIndex.x;
+        int gridY = gridIndex.y;
+        //Gives position of room based on grid index
+        return new Vector3(roomWidth * (gridX - gridSizeX / 2 ), roomHeight * (gridY - gridSizeY /2));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color gizmoColor = new Color(0, 1, 1, 0.05f);
+        Gizmos.color = gizmoColor;
+
+        for(int x = 0; x < gridSizeX; x++) 
+        {
+            for(int y = 0; y < gridSizeY; y++) 
+            {
+                Vector3 position = GetPositionFromGridIndex(new Vector2Int(x, y));
+                Gizmos.DrawWireCube(position, new Vector3(roomWidth, roomHeight, 1));
+            }
+        }
     }
 }
