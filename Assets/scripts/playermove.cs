d4f58aa6e9c30e92184ade75661f275ce38bf6f7
Ivan <s107917@ltisdschools.net>
2024-04-09 16:05:45 -0500

    starting player movement

--- Assets/scripts/playermove.cs
+++ Assets/scripts/playermove.cs
@@ -4,7 +4,38 @@ using UnityEngine;
 
 public class playermove : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public float horizontal;
+    public float vertical;
+    public float speed = 2f;
+    public bool FacingLeft = true;
 
-    
+    public Rigidbody2D rb;
+    public Transform tf;
+
+    public void Update()
+    {
+        horizontal = Input.GetAxisRaw("horizontal");
+        vertical = Input.GetAxisRaw("vertical");
+
+        if (Input.GetButton )
+        flip();
+    }
+
+    public void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (horizontal * speed, vertical * speed);
+
+    }
+
+    private void flip()
+    {
+        if (!FacingLeft && horizontal < 0f || FacingLeft && horizontal > 0f)
+        {
+            FacingLeft = !FacingLeft;
+            Vector3 v3 = transform.localScale;
+            v3.x *= -1f;
+            transform.localScale = v3;
+        }
+    }
+
 }
