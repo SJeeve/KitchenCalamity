402dcc4005e07c2595c164a02456d0778db1825e
Ivan <s107917@ltisdschools.net>
2024-04-19 16:09:35 -0500

    finish health bar prototype

--- Assets/scripts/healthbar.cs
+++ Assets/scripts/healthbar.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 public class healthbar : MonoBehaviour
 {
     public Slider healthbarSlider;
-
+    public Gradient grad;
+    public Image fill;
     public void SetMaxHealth(int health)
     {
         healthbarSlider.maxValue = health;
         healthbarSlider.value = health;
+        fill.color = grad.Evaluate(1f);
     }
     public void setHealth(int health)
     {
         healthbarSlider.value = health;
+        fill.color = grad.Evaluate(healthbarSlider.normalizedValue);
     }
 }
