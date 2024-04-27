21fac5f2672fda0915dc68671778856b80c2d573
Owen <ofsmithstar@gmail.com>
2024-04-26 22:17:28 -0500

    Fixed bug where room would generate in same place twice, workinon sword hitbox

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -7,11 +7,25 @@ public class BreakableObject : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private SpriteRenderer spriteRenderer;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public float Health
     {
-        if(collision.collider.tag == "Player")
+        set
         {
-                Destroy(gameObject);               
+            _health = value;
+
+            if (_health <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
+        get { return _health; }
+    }
+
+    public float _health = 1;
+    
+    void OnHit(float damage)
+    {
+        Debug.Log("Breakable object hit");
+        Health -= damage;
     }
 }
--- Assets/puddleScript.cs
+++ Assets/puddleScript.cs
@@ -5,7 +5,25 @@ using UnityEngine;
 public class puddleScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void OnHit(float damage) {
+    [SerializeField] public Animator animator;
+    public float Health
+    {
+        set
+        {
+            _health = value;
+
+            if (_health <= 0)
+            {
+                animator.SetTrigger("Death");
+            }
+        }
+        get { return _health; }
+    }
+
+    public float _health = 3;
+
+    void OnHit(float damage, float id) {
         Debug.Log("Puddle hit for " + damage);
+        Health -= damage;
     }
 }
