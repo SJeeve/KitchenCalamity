a70f924f411e4b4a7868767a3f2731da09783696
Owen <ofsmithstar@gmail.com>
2024-04-26 22:30:02 -0500

    fiddled with animators for puddle

--- Assets/puddleScript.cs
+++ Assets/puddleScript.cs
@@ -6,23 +6,32 @@ public class puddleScript : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] public Animator animator;
+
+    bool Alive = true;
     public float Health
     {
         set
         {
+            if(value < _health)
+            {
+                animator.SetTrigger("Hit");
+            }
             _health = value;
 
             if (_health <= 0)
             {
-                animator.SetTrigger("Death");
+                animator.SetBool("Alive", false);
             }
         }
         get { return _health; }
     }
 
     public float _health = 3;
-
-    void OnHit(float damage, float id) {
+    public void Start()
+    {
+        animator.SetBool("Alive", true);
+    }
+    void OnHit(float damage) {
         Debug.Log("Puddle hit for " + damage);
         Health -= damage;
     }
