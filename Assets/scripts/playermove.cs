fe6acb46df2bad044c657e50f555409df1d68349
Ivan <s107917@ltisdschools.net>
2024-05-03 16:00:39 -0500

    finished and colored run idle attack animations in sprite sheet

--- Assets/scripts/playermove.cs
+++ Assets/scripts/playermove.cs
@@ -8,6 +8,7 @@ public class playermove : MonoBehaviour
     public float movespeed;
     public Rigidbody2D rb;
     private Vector2 input;
+    public Animation ani;
     
     private void Update()
     {
@@ -15,7 +16,10 @@ public class playermove : MonoBehaviour
         input.y = Input.GetAxisRaw("Vertical");
         input.Normalize();
         rb.velocity = input * movespeed * Time.deltaTime;
-
+        if (input.x > 1 || input.y > 1)
+        {
+            ani.Play();
+        }
         
     }
 
